c6107a5981fff9af611d691e66a43c33368229f3
el16dmcr <el16dmcr@leeds.ac.uk>
2025-03-19 13:24:10 +0000

    Created a quick template for a QP tracking controller to be complted later

---
+++ include/panda_QP_controller.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <controller_interface/multi_interface_controller.h>
+#include <hardware_interface/robot_hw.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <franka_hw/franka_model_interface.h>
+#include <franka_hw/franka_state_interface.h>
+#include <franka_hw/franka_cartesian_command_interface.h>
+#include <ros/node_handle.h>
+#include <ros/time.h>
+#include <std_msgs/Float64MultiArray.h>
+#include <Eigen/Core>
+
+namespace panda_controllers {
+#define NUM_JOINTS 7
+
+    class QPController : public controller_interface::MultiInterfaceController<
+            franka_hw::FrankaModelInterface,
+            franka_hw::FrankaStateInterface,
+            hardware_interface::EffortJointInterface> {
+    public:
+        bool init(hardware_interface::RobotHW *robot_hw, ros::NodeHandle &node_handle) override;
+
+        void update(const ros::Time &time, const ros::Duration &period) override;
+
+        void starting(const ros::Time &time) override;
+
+    private:
+//        void desiredStateCallback(const panda_inverse_dynamics_controller::DesiredState::ConstPtr& msg);
+
+        ros::Subscriber state_subscriber_;
+
+        std::unique_ptr <franka_hw::FrankaModelHandle> model_handle_;
+        std::unique_ptr <franka_hw::FrankaStateHandle> state_handle_;
+
+        std::vector<hardware_interface::JointHandle> joint_handles_;
+
+
+    };
+} // namespace panda_controllers
