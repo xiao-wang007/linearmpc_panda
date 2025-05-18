f0abc4ad8bc3ae9f12c0764d82b7b828654a902e
xiao-wang007 <xwang0505dazzleyou@gmail.com>
2025-05-18 21:52:04 +0000

    next up: multithreading in executor node for u_cmd_spline_ and multithreading for solve_publish_and_update()

--- include/linearmpc_panda/mpc_solver_node.h
+++ include/linearmpc_panda/mpc_solver_node.h
@@ -7,113 +7,128 @@
 #include <ros/ros.h>
 #include <linearmpc_panda/StampedFloat64MultiArray.h>
 #include <sensor_msgs/JointState.h>
+#include <std_msgs/Time.h>
+#include <thread>
+#include <atomic>
 
-#define NUM_JOINTS 7
-#define PI 3.14
+namespace MPCControllers
+{
+    // Constants
+    #define NUM_JOINTS 7
+    #define PI 3.14
 
-using drake::multibody::MultibodyPlant;
-using drake::multibody::Parser;
-using drake::systems::Context;
-using drake::systems::System;
-using drake::AutoDiffXd;
-using drake::math::InitializeAutoDiff;
-using drake::multibody::AddMultibodyPlantSceneGraph;
-using drake::multibody::MultibodyPlant;
-using drake::multibody::Parser;
-using drake::multibody::ModelInstanceIndex;
-using drake::math::RollPitchYaw;
-using drake::math::RigidTransform;
-using drake::math::RotationMatrix;
-using drake::multibody::Frame;
-using drake::multibody::SpatialVelocity;
-using drake::trajectories::PiecewisePolynomial;
+    using drake::multibody::MultibodyPlant;
+    using drake::multibody::Parser;
+    using drake::systems::Context;
+    using drake::systems::System;
+    using drake::AutoDiffXd;
+    using drake::math::InitializeAutoDiff;
+    using drake::multibody::AddMultibodyPlantSceneGraph;
+    using drake::multibody::MultibodyPlant;
+    using drake::multibody::Parser;
+    using drake::multibody::ModelInstanceIndex;
+    using drake::math::RollPitchYaw;
+    using drake::math::RigidTransform;
+    using drake::math::RotationMatrix;
+    using drake::multibody::Frame;
+    using drake::multibody::SpatialVelocity;
+    using drake::trajectories::PiecewisePolynomial;
 
-using namespace drake; // for all eigen types
-using namespace drake::solvers;
+    using namespace drake; // for all eigen types
+    using namespace drake::solvers;
 
-class MPCSolverNode {
-public:
-    //
-    MPCSolverNode();
+//########################################################################################
+    class MPCSolverNode 
+    {
+    public:
+        //
+        MPCSolverNode();
 
-    /* will not work as this node is not exposed to controller interface 
-       I may have to create a publisher for panda hardware states in my
-       QPController interface */ 
-    //MPCSolverNode(const Eigen::VectorXd& q_now,
-                  //const Eigen::VectorXd& v_now);
+        /* will not work as this node is not exposed to controller interface 
+        I may have to create a publisher for panda hardware states in my
+        QPController interface */ 
+        //MPCSolverNode(const Eigen::VectorXd& q_now,
+                    //const Eigen::VectorXd& v_now);
 
-    //
-    ~MPCSolverNode() = default;
-    
-    //
-    void run();
+        //
+        ~MPCSolverNode() = default;
+        
+        //
+        void run();
 
-    // Function to solve the MPC problem
-    void solve_and_update(const ros::TimerEvent& event);
+        //
+        void get_mpc_start_time(const std_msgs::Time::ConstPtr& msg);
 
-    //
-    void joint_state_callback_sim(const sensor_msgs::JointState::ConstPtr& msg);
-    
-    //
-    void joint_state_callback_HW(const sensor_msgs::JointState::ConstPtr& msg);
+        // Function to solve the MPC problem
+        void solve_publish_and_update();
 
-private:
-    // ROS related member variables
-    ros::NodeHandle nh_;
-    ros::Timer solver_timer_;
-    ros::Publisher mpc_sol_pub_;
-    ros::Subscriber state_sub_;
-    linearmpc_panda::StampedFloat64MultiArray mpc_sol_msg_;
+        //
+        void joint_state_callback_sim(const sensor_msgs::JointState::ConstPtr& msg);
+        
+        //
+        void joint_state_callback_HW(const sensor_msgs::JointState::ConstPtr& msg);
 
-    //drake::systems::DiscreteStateIndex state_index_;
-    bool do_sim_ {true}; 
-    std::string panda_file_ {"/home/rosdrake/panda_arm.urdf"};
-    std::string integrator_ {"RK4"};
-    drake::math::RigidTransform<double> X_W_base_ {};
-    int nx_ {14}; 
-    int nq_ {7};
-    int nu_ {7}; 
-    int Nt_ {5}; 
-    int n_exe_steps_ {1};
-    double h_env_ {0.001}; //sim and panda control freq
-    double h_mpc_{0.04};
-    int Nh_ {};
-    double execution_length_ {};
-    double mpc_horizon_ {};
-    Eigen::MatrixXd Q_;
-    Eigen::MatrixXd R_;
-    Eigen::MatrixXd P_;
-    Eigen::VectorXd udot_up_ {Eigen::VectorXd::Constant(nu_, 1000.0)}; 
-    Eigen::VectorXd udot_low_ {-udot_up_}; 
-    Eigen::MatrixXd xref_now_ {};
-    Eigen::MatrixXd uref_now_ {};
-    Eigen::Matrix<double, NUM_JOINTS, 1> q_now_ {};
-    Eigen::Matrix<double, NUM_JOINTS, 1> v_now_ {};
-    Eigen::Matrix<double, NUM_JOINTS, 1> u_now_ {};
+    private:
+        // ROS related member variables
+        ros::NodeHandle nh_;
+        ros::Timer solver_timer_;
+        ros::Publisher mpc_sol_pub_;
+        ros::Subscriber state_sub_;
+        ros::Subscriber mpc_start_time_;
+        ros::Time t_mpc_start_;
+        linearmpc_panda::StampedFloat64MultiArray latest_mpc_sol_msg_;
 
-    //member variables related to the reference traj
-    int N_ = 60;
-    std::vector<std::string> var_names_ = {"q_panda", "v_panda", "us", "fn", "ft", "v1_post", "w1_post", "h"};
-    std::vector<int> dims_ = {7, 7, 7, 1, 2, 2, 1, 1};
-    std::vector<int> times_ = {N_, N_, N_, 1, 1, 1, 1, N_-1};
-    std::string ref_traj_path_ = "/home/rosdrake/src/src/mpc/traj_refs/1.npy";
-    MyUtils::ProcessedSolution data_proc_; 
+        //drake::systems::DiscreteStateIndex state_index_;
+        bool do_sim_ {true}; 
+        std::string panda_file_ {"/home/rosdrake/panda_arm.urdf"};
+        std::string integrator_ {"RK4"};
+        drake::math::RigidTransform<double> X_W_base_ {};
+        int nx_ {14}; 
+        int nq_ {7};
+        int nu_ {7}; 
+        int Nt_ {5}; 
+        int n_exe_steps_ {1};
+        double h_env_ {0.001}; //sim and panda control freq
+        double h_mpc_{0.04};
+        int Nh_ {};
+        double execution_length_ {};
+        double mpc_horizon_ {};
+        Eigen::MatrixXd Q_;
+        Eigen::MatrixXd R_;
+        Eigen::MatrixXd P_;
+        Eigen::VectorXd udot_up_ {Eigen::VectorXd::Constant(nu_, 1000.0)}; 
+        Eigen::VectorXd udot_low_ {-udot_up_}; 
+        Eigen::MatrixXd xref_now_ {};
+        Eigen::MatrixXd uref_now_ {};
+        Eigen::Matrix<double, NUM_JOINTS, 1> q_now_ {};
+        Eigen::Matrix<double, NUM_JOINTS, 1> v_now_ {};
+        Eigen::Matrix<double, NUM_JOINTS, 1> u_now_ {};
 
-    AutoDiffVecXd f_grad_;
-    PiecewisePolynomial<double> x_ref_spline_;
-    PiecewisePolynomial<double> u_ref_spline_;
+        //member variables related to the reference traj
+        int N_ = 60;
+        std::vector<std::string> var_names_ = {"q_panda", "v_panda", "us", "fn", "ft", "v1_post", "w1_post", "h"};
+        std::vector<int> dims_ = {7, 7, 7, 1, 2, 2, 1, 1};
+        std::vector<int> times_ = {N_, N_, N_, 1, 1, 1, 1, N_-1};
+        std::string ref_traj_path_ = "/home/rosdrake/src/src/mpc/traj_refs/1.npy";
+        MyUtils::ProcessedSolution data_proc_; 
 
-    /* Now I learnt, using unique_ptr avoids DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN assertion */
-    std::unique_ptr<MPCControllers::LinearMPCProb> prob_;
+        AutoDiffVecXd f_grad_;
+        PiecewisePolynomial<double> x_ref_spline_;
+        PiecewisePolynomial<double> u_ref_spline_;
 
-    // run-time member data
-    ros::Time t_now_ {};
-    //Eigen::VectorXd state_now_ {}; bad practic without initialization as size is not know, can't do << q_now_, v_now_ without resizing
-    //Eigen::Matrix<double, NUM_JOINTS*2, 1> state_now_ {};  have to use macros since I cannot use nu_ here
-    Eigen::VectorXd state_now_ {}; // initialize in constructor
-    Eigen::MatrixXd u_ref_cmd_ {}; // initialize in constructor
+        /* Now I learnt, using unique_ptr avoids DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN assertion */
+        std::unique_ptr<MPCControllers::LinearMPCProb> prob_;
 
-    // Mutex for thread safety
-    std::mutex mpc_mutex_;
-    ros::Time t_init_node_ {};
-};
+        // run-time member data
+        //Eigen::VectorXd state_now_ {}; bad practic without initialization as size is not know, can't do << q_now_, v_now_ without resizing
+        //Eigen::Matrix<double, NUM_JOINTS*2, 1> state_now_ {};  have to use macros since I cannot use nu_ here
+        Eigen::VectorXd state_now_ {}; // initialize in constructor
+        Eigen::MatrixXd u_ref_cmd_ {}; // initialize in constructor
+
+        // Mutex for thread safety
+        std::mutex mpc_mutex_;
+        double current_time_ {}; //w.r.t to mpc start time
+        ros::Time t_now_
+    };
+
+} // namespace MPCControllers
--- src/mpc_solver_node.cc
+++ src/mpc_solver_node.cc
@@ -1,146 +1,160 @@
 // #include "mpc_solver_node.h"
 #include <linearmpc_panda/mpc_solver_node.h>
 
-//###############################################################################
-MPCSolverNode::MPCSolverNode() 
+namespace MPCControllers
 {
-    ros::Time t_begin = ros::Time::now();
-    std::cout << '\n' << std::endl;
-    ROS_INFO("Starting init of mpc_solver_node! taken %f seconds", t_begin.toSec());
-    std::cout << '\n' << std::endl;
+    //###############################################################################
+    MPCSolverNode::MPCSolverNode() 
+    {
+        //solver_timer_ = nh.createTimer(ros::Duration(0.01), &MPCSolverNode::solve_and_update, this); // 100Hz
+        mpc_sol_pub_ = nh_.advertise<linearmpc_panda::StampedFloat64MultiArray>("mpc_sol", 1);
 
-    // Initialize the ROS node
-    ros::NodeHandle nh;
-    solver_timer_ = nh.createTimer(ros::Duration(0.01), &MPCSolverNode::solve_and_update, this); // 100Hz
-    mpc_sol_pub_ = nh.advertise<linearmpc_panda::StampedFloat64MultiArray>("mpc_sol", 1);
+        mpc_start_time_ = nh_.subscribe("mpc_t_start", 1, &MPCSolverNode::get_mpc_start_time, this); //True for latched publisher
 
-	/* Direct access to panda's current state, no need to use a sub, but here in gazebo, I need to do this through ros */
-	//get panda state, topic belongs to franka_gazebo, which operates as 1kHz, then the callback is also called at 1kHz
-	state_sub_ = nh.subscribe("joint_states", 1, &MPCSolverNode::joint_state_callback_sim, this);
+        /* Direct access to panda's current state, no need to use a sub, but here in gazebo, I need to do this through ros */
+        //get panda state, topic belongs to franka_gazebo, which operates as 1kHz, then the callback is also called at 1kHz
+        state_sub_ = nh_.subscribe("joint_states", 1, &MPCSolverNode::joint_state_callback_sim, this);
 
-    /*TODO: create the publisher in QPController interface to publish panda hardware current state*/
-	//state_sub_ = nh.subscribe("joint_states_pandaHW", 1, &MPCSolverNode::joint_state_callback_HW, this);
+        /*TODO: create the publisher in QPController interface to publish panda hardware current state*/
+        //state_sub_ = nh.subscribe("joint_states_pandaHW", 1, &MPCSolverNode::joint_state_callback_HW, this);
 
-	// compute mpc related parameters, this has to go first as init_prog() uses them
-	Nh_ = Nt_ - 1;
-	execution_length_ = h_mpc_ * n_exe_steps_;
-	mpc_horizon_ = h_mpc_ * Nh_;
+        // compute mpc related parameters, this has to go first as init_prog() uses them
+        Nh_ = Nt_ - 1;
+        execution_length_ = h_mpc_ * n_exe_steps_;
+        mpc_horizon_ = h_mpc_ * Nh_;
 
-	//define the meta data of a std_msgs::Float64MultiArray for mpc_solution
-	mpc_sol_msg_.data.layout.dim.resize(2);
-	mpc_sol_msg_.data.layout.dim[0].label = "rows";
-	mpc_sol_msg_.data.layout.dim[0].size = nu_;
-	mpc_sol_msg_.data.layout.dim[0].stride = nu_ * Nh_; // assuming row-major here
-	mpc_sol_msg_.data.layout.dim[1].label = "cols";
-	mpc_sol_msg_.data.layout.dim[1].size = Nh_;
-	mpc_sol_msg_.data.layout.dim[1].stride = Nh_;
-	mpc_sol_msg_.data.data.resize(nu_ * Nh_);  // Preallocate
+        //define the meta data of a std_msgs::Float64MultiArray for mpc_solution
+        latest_mpc_sol_msg_.data.layout.dim.resize(2);
+        latest_mpc_sol_msg_.data.layout.dim[0].label = "rows";
+        latest_mpc_sol_msg_.data.layout.dim[0].size = nu_;
+        latest_mpc_sol_msg_.data.layout.dim[0].stride = nu_ * Nh_; // assuming row-major here
+        latest_mpc_sol_msg_.data.layout.dim[1].label = "cols";
+        latest_mpc_sol_msg_.data.layout.dim[1].size = Nh_;
+        latest_mpc_sol_msg_.data.layout.dim[1].stride = Nh_;
+        latest_mpc_sol_msg_.data.data.resize(nu_ * Nh_);  // Preallocate
 
 
-    // load x_ref and u_ref
-    data_proc_ = MyUtils::ProcessSolTraj(ref_traj_path_ , var_names_, dims_, times_);
+        // load x_ref and u_ref
+        data_proc_ = MyUtils::ProcessSolTraj(ref_traj_path_ , var_names_, dims_, times_);
 
-    //init MyControllers::LinearMPCProb() here
-    X_W_base_ = RigidTransform<double>(RollPitchYaw<double>(Vector3<double>(0., 0., -90.) * PI / 180.),
-                                    Vector3<double>(0., -0.2, 0.));
+        //init MyControllers::LinearMPCProb() here
+        X_W_base_ = RigidTransform<double>(RollPitchYaw<double>(Vector3<double>(0., 0., -90.) * PI / 180.),
+                                        Vector3<double>(0., -0.2, 0.));
 
-    //make Q
-    Eigen::VectorXd q_coef = Eigen::VectorXd::Constant(nu_, 200.0) * 12.;
-    Eigen::VectorXd v_coef = Eigen::VectorXd::Constant(nu_, 1.) * 0.1;
-    Eigen::VectorXd Q_diags(q_coef.rows() + v_coef.rows());
-    Q_diags.head(q_coef.rows()) = q_coef;
-    Q_diags.tail(v_coef.rows()) = v_coef;
-    Eigen::DiagonalMatrix<double, NUM_JOINTS*2> Q_sparse = Q_diags.asDiagonal();
-    Eigen::MatrixXd Q = Q_sparse.toDenseMatrix();
+        //make Q
+        Eigen::VectorXd q_coef = Eigen::VectorXd::Constant(nu_, 200.0) * 12.;
+        Eigen::VectorXd v_coef = Eigen::VectorXd::Constant(nu_, 1.) * 0.1;
+        Eigen::VectorXd Q_diags(q_coef.rows() + v_coef.rows());
+        Q_diags.head(q_coef.rows()) = q_coef;
+        Q_diags.tail(v_coef.rows()) = v_coef;
+        Eigen::DiagonalMatrix<double, NUM_JOINTS*2> Q_sparse = Q_diags.asDiagonal();
+        Eigen::MatrixXd Q = Q_sparse.toDenseMatrix();
 
-    //make R
-    Eigen::VectorXd u_coef = Eigen::VectorXd::Constant(nu_, 0.001);
-    Eigen::MatrixXd R = u_coef.asDiagonal();
+        //make R
+        Eigen::VectorXd u_coef = Eigen::VectorXd::Constant(nu_, 0.001);
+        Eigen::MatrixXd R = u_coef.asDiagonal();
 
-    //make P
-    Eigen::MatrixXd P = Eigen::MatrixXd::Identity(nx_, nx_) * 100;
+        //make P
+        Eigen::MatrixXd P = Eigen::MatrixXd::Identity(nx_, nx_) * 100;
 
-    /* have to do this to avoid DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN() assertion as 
-        I have a MultibodyPlant() inside LinearMPCProb()*/
-    prob_ = std::make_unique<MPCControllers::LinearMPCProb>(panda_file_, integrator_, nx_, nu_, execution_length_, 
-                                                           h_mpc_, h_env_, Nt_, X_W_base_, Q, R, P, 
-                                                           data_proc_.x_ref_spline, 
-                                                           data_proc_.u_ref_spline); 
-    //init solver output
-    state_now_ = Eigen::VectorXd::Zero(nx_);
-    u_ref_cmd_ = Eigen::MatrixXd::Zero(nu_, Nh_);
+        /* have to do this to avoid DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN() assertion as 
+            I have a MultibodyPlant() inside LinearMPCProb()*/
+        prob_ = std::make_unique<MPCControllers::LinearMPCProb>(panda_file_, integrator_, nx_, nu_, execution_length_, 
+                                                            h_mpc_, h_env_, Nt_, X_W_base_, Q, R, P, 
+                                                            data_proc_.x_ref_spline, 
+                                                            data_proc_.u_ref_spline); 
+        //init solver output
+        state_now_ = Eigen::VectorXd::Zero(nx_);
+        u_ref_cmd_ = Eigen::MatrixXd::Zero(nu_, Nh_);
 
-    //print the initial position
-    std::cout << "q0: \n" << data_proc_.x_ref_spline.value(0.).transpose() << std::endl;
+        //print the initial position
+        std::cout << "q0: \n" << data_proc_.x_ref_spline.value(0.).transpose() << std::endl;
 
-    t_init_node_ = ros::Time::now();
-    ROS_INFO("mpc_solver_node inited! taken %f seconds", (t_init_node_ - t_begin).toSec());
-}
+    }
 
-//########################################################################################
-void MPCSolverNode::run() 
-{
-    ros::spin();
-}
+    //########################################################################################
+    void MPCSolverNode::run() 
+    {
+        while (ros::ok()) 
+        {
+            // Call the solve_and_update function at a fixed rate
+            solve_publish_and_update();
 
-//########################################################################################
-void MPCSolverNode::solve_and_update(const ros::TimerEvent& event) 
-{
-    // Lock the mutex to ensure thread safety
-    std::lock_guard<std::mutex> lock(mpc_mutex_);
+            // no sleep, event driven
+            //ros::Duration(0.01).sleep(); // 100Hz
+        ros::spinOnce();
+        }
+    }
 
-    ////get current reference trajectory for debugging 
-    //t_now_ = ros::Time::now();
-    //auto t_start = (t_now_-t_init_node_).toSec();
-    //auto ts_ = Eigen::VectorXd::LinSpaced(Nt_, t_start, t_start+mpc_horizon_);
-    //xref_now_ = data_proc_.x_ref_spline.vector_values(ts_);
-    //uref_now_ = data_proc_.u_ref_spline.vector_values(ts_);
+    void MPCSolverNode::get_mpc_start_time(const std_msgs::Time::ConstPtr& msg) 
+    {
+        // Lock the mutex to ensure thread safety
+        std::lock_guard<std::mutex> lock(mpc_mutex_);
+        t_mpc_start_ = msg->data;
+        ROS_INFO("mpc_solver_node::get_mpc_start_time() runs once");
+    }
 
-    //std::cout << "uref_now_: \n" << uref_now_ << std::endl;
+    //########################################################################################
+    void MPCSolverNode::solve_publish_and_update() 
+    {
+        // Lock the mutex to ensure thread safety
+        std::lock_guard<std::mutex> lock(mpc_mutex_);
 
-    //call the mpc solver
-    state_now_ << q_now_, v_now_; // these are updated in the subcription callback
-    t_now_ = ros::Time::now();
-    prob_->Solve_and_update_C_d_for_solver_errCoord(state_now_, t_now_);
+        ////get current reference trajectory for debugging 
+        //t_now_ = ros::Time::now();
+        //auto t_start = (t_now_-t_init_node_).toSec();
+        //auto ts_ = Eigen::VectorXd::LinSpaced(Nt_, t_start, t_start+mpc_horizon_);
+        //xref_now_ = data_proc_.x_ref_spline.vector_values(ts_);
+        //uref_now_ = data_proc_.u_ref_spline.vector_values(ts_);
 
-    //get time here for stamping the message
-    mpc_sol_msg_.header.stamp = ros::Time::now();
-    mpc_sol_msg_.header.frame_id = "mpc_sol";
+        //std::cout << "uref_now_: \n" << uref_now_ << std::endl;
 
-    //save solution to member variable u_ref_cmd_
-    prob_->Get_solution(u_ref_cmd_); //Pass by argument
+        //call the mpc solver
+        state_now_ << q_now_, v_now_; // these are updated in the subcription callback
+        t_now_ = ros::Time::now();
+        current_time_ = (t_mpc_start_ - t_now_).toSec();
+        prob_->Solve_and_update_C_d_for_solver_errCoord(state_now_, t_now_);
 
-    std::cout << "u_ref_cmd_ of shape: (" << u_ref_cmd_.rows() << ", " << u_ref_cmd_.cols() << ")" << std::endl;
-    std::cout << "u_ref_cmd_: \n" << u_ref_cmd_ << std::endl;
+        //get time here for stamping the message
+        latest_mpc_sol_msg_.header.stamp = ros::Time::now();
+        latest_mpc_sol_msg_.header.frame_id = "mpc_sol";
 
-    //map solution to linearmpc_panda::StampedFloat64MultiArray 
-    Eigen::Map<Eigen::MatrixXd>(mpc_sol_msg_.data.data.data(), nu_, Nh_) = u_ref_cmd_;
+        //save solution to member variable u_ref_cmd_
+        prob_->Get_solution(u_ref_cmd_); //Pass by argument
 
-    //publish the solution message
-    mpc_sol_pub_.publish(mpc_sol_msg_);
+        std::cout << "u_ref_cmd_ of shape: (" << u_ref_cmd_.rows() << ", " << u_ref_cmd_.cols() << ")" << std::endl;
+        std::cout << "u_ref_cmd_: \n" << u_ref_cmd_ << std::endl;
 
-    ROS_INFO("mpc_solver_node::solve_and_update() runs once");
-}
+        //map solution to linearmpc_panda::StampedFloat64MultiArray 
+        Eigen::Map<Eigen::MatrixXd>(latest_mpc_sol_msg_.data.data.data(), nu_, Nh_) = u_ref_cmd_;
 
-//########################################################################################
-void MPCSolverNode::joint_state_callback_sim(const sensor_msgs::JointState::ConstPtr& msg) 
-{
-    //store current state
-    // get the current joint position and velocity
-    q_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->position.data());
-    v_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->velocity.data());
-    //u_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->effort.data());
-}
+        //publish the solution message
+        mpc_sol_pub_.publish(latest_mpc_sol_msg_);
 
-//########################################################################################
-void MPCSolverNode::joint_state_callback_HW(const sensor_msgs::JointState::ConstPtr& msg) 
-{
-    //store current state
-    // get the current joint position and velocity
-    q_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->position.data());
-    v_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->velocity.data());
-    //u_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->effort.data());
-}
+        ROS_INFO("mpc_solver_node::solve_and_update() runs once");
+    }
+
+    //########################################################################################
+    void MPCSolverNode::joint_state_callback_sim(const sensor_msgs::JointState::ConstPtr& msg) 
+    {
+        //store current state
+        // get the current joint position and velocity
+        q_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->position.data());
+        v_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->velocity.data());
+        //u_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->effort.data());
+    }
+
+    //########################################################################################
+    void MPCSolverNode::joint_state_callback_HW(const sensor_msgs::JointState::ConstPtr& msg) 
+    {
+        //store current state
+        // get the current joint position and velocity
+        q_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->position.data());
+        v_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->velocity.data());
+        //u_now_ = Eigen::Map<const Eigen::Matrix<double, NUM_JOINTS, 1>>(msg->effort.data());
+    }
+
+} // namespace MPCControllers
 
 
 //########################################################################################
@@ -150,7 +164,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "mpc_solver_node");
     
     // Create an instance of the MPCSolverNode class
-    MPCSolverNode mpc_solver_node;
+    MPCControllers::MPCSolverNode mpc_solver_node;
     
     // Run the node
     mpc_solver_node.run();
